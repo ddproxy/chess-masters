c4ef64f5fcb0170de6d34f86661d24dde71f7cf6
Tosin <midastouchproductions@gmail.com>
2016-09-01 14:31:12 -0500

    grab background for tiles

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('booya');
+$(document).ready(function() {
+    var $board = $('.board');
+    var id = 81;
+// make the board
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            $board.append('<div class="square" id="' + id + '"> ' + id + ' </div>');
+            id++;
+        };
+        id = id - 18;
+    };
+
+// give it the right background
+
+
+
+
+
+
+
+
+
+
+
+
+});
